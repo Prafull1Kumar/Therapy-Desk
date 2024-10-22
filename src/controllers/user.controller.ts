eda7f135d03e42119807603be86c3c9d4fdec24b
Prafull <prafull@arth.team>
2024-10-21 20:05:49 -0500

    created auth controller

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -12,16 +12,13 @@ import {
   del,
   get,
   getModelSchemaRef,
-  HttpErrors,
   param,
   patch,
-  post,
   put,
   requestBody,
-  response,
+  response
 } from '@loopback/rest';
 import _ from 'lodash';
-import {AWS_LAMBDA_FUNCTIONS} from '../keys';
 import {User} from '../models';
 import {OrganizationRepository, RoleRepository, UserRepository} from '../repositories';
 import {AwsLambdaService, UserManagementService} from '../services';
@@ -41,111 +38,6 @@ export class UserController {
     // @inject(SecurityBindings.USER)
     // public currentUserProfile: UserProfile,
   ) { }
-
-  @post('/users/reset-password/init', {
-    responses: {
-      '200': {
-        description: 'Confirmation that reset password email has been sent',
-      },
-    },
-  })
-  async resetPasswordInit(
-    @requestBody() resetPasswordInit: any,
-  ): Promise<any> {
-    if (this.isValidEmail(resetPasswordInit.email)) {
-      throw new HttpErrors.UnprocessableEntity("Error. Please check your email address.");
-    }
-
-    //To Do
-    const password_reset = await this.userManagementService.requestPasswordReset( //WORK_HERE
-      resetPasswordInit.email
-    );
-    // if (password_reset.SendTemplatedEmailResponse) {
-    if (password_reset?.ResponseMetadata?.HTTPStatusCode == 200) {
-      return {success: true, message: "Successfully sent reset password link"};
-    }
-    else {
-      throw new HttpErrors.InternalServerError(
-        "Something went wrong. Please try again or contact your administrator.",
-      );
-    }
-  }
-
-  @post('/users/resend-email/init')
-  @response(200, {
-    description: 'Email Resend',
-    content: {'application/json': {schema: getModelSchemaRef(User)}},
-  })
-  async resendEmail(): Promise<void> {
-    this.awsLambdaService.invokeFunction(AWS_LAMBDA_FUNCTIONS.welcome, {})
-      .then((data) => {
-        console.log(data);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-    return;
-  }
-
-  @post('/user')
-  @response(200, {
-    description: 'User model instance',
-    content: {'application/json': {schema: getModelSchemaRef(User)}},
-  })
-  async create(
-    @requestBody({
-      content: {
-        'application/json': {
-          schema: getModelSchemaRef(User, {
-            title: 'NewUser',
-            exclude: ['id'],
-          }),
-        },
-      },
-    })
-    user: Omit<User, 'id'>,
-  ): Promise<User> {
-    const transaction = await this.userRepository.dataSource.beginTransaction(IsolationLevel.READ_COMMITTED);
-    try {
-
-      const existingUser = await this.userRepository.findOne({where: {email: user.email.toLocaleLowerCase()}});
-      if (existingUser)
-        throw new HttpErrors.Forbidden('Error. This email address already exists.');
-
-      const email = user.email.toLocaleLowerCase();
-      if (email && !this.isValidEmail(email)) {
-        console.error("The email address is invalid.");
-        throw new HttpErrors.BadRequest("The email address is invalid.");
-      }
-
-      const password = user.password;
-      delete user.password;
-      // const password = generatePassword.generate({
-      //   length: 8,
-      //   numbers: true,
-      // });
-
-      user.status = 'PROCESSING'
-
-      let newUser = await this.userRepository.create(user, {transaction});
-
-      await this.userManagementService.createUserCredentials(Object.assign(newUser, {
-        password: password,
-        user_id: newUser.id
-      }), transaction);
-      await transaction.commit();
-      return newUser
-    } catch (error) {
-      await transaction.rollback();
-      throw error;
-    }
-  }
-
-  isValidEmail(email): Boolean {
-    // Regular expression for validating an email address
-    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return regex.test(email);
-  }
 
   @get('/user/count')
   @response(200, {
